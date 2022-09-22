bdaac2d9fcd15490107f8f53e9b2f64e83c035e7
Tolunay Gençman <tolunaygencman@hotmail.com>
2022-09-22 21:53:35 +0300

    -Added Repositories for DataAccess Layer

---
+++ MenuApp/MenuApp.DataAccess/EntityFrameWork/Concretes/FoodRepository.cs
@@ -0,0 +1,14 @@
+﻿using MenuApp.Core.DataAccess.Concretes;
+using MenuApp.DataAccess.Abstracts;
+using MenuApp.DataAccess.EntityFrameWork.Context;
+using MenuApp.Entity.Concretes;
+
+namespace MenuApp.DataAccess.EntityFrameWork.Concretes
+{
+    public class FoodRepository : GenericRepository<Food, MenuAppDbContext>, IFoodRepository
+    {
+        public FoodRepository(MenuAppDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ MenuApp/MenuApp.DataAccess/EntityFrameWork/Concretes/MenuSettingRepository.cs
@@ -0,0 +1,14 @@
+﻿using MenuApp.Core.DataAccess.Concretes;
+using MenuApp.DataAccess.Abstracts;
+using MenuApp.DataAccess.EntityFrameWork.Context;
+using MenuApp.Entity.Concretes;
+
+namespace MenuApp.DataAccess.EntityFrameWork.Concretes
+{
+    public class MenuSettingRepository : GenericRepository<MenuSetting, MenuAppDbContext>, IMenuSettingRepository
+    {
+        public MenuSettingRepository(MenuAppDbContext context) : base(context)
+        {
+        }
+    }
+}
