aff07eb58a5e9841cdc4e2b0931b5c36678d023c
Tolunay Gençman <tolunaygencman@hotmail.com>
2022-11-17 21:54:11 +0300

    *Added Fluent Validator startup services *Added layout for member and admin areas

---
+++ MenuApp/MenuApp.Business/Abstracts/IMemberService.cs
@@ -0,0 +1,14 @@
+﻿using MenuApp.Business.DTOs.Members;
+using MenuApp.Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuApp.Business.Abstracts
+{
+    public interface IMemberService
+    {
+        Task<IDataResult<MemberDto>> GetByIdentityId(string identityId);
+    }
+}
---
+++ MenuApp/MenuApp.Business/Concretes/MemberManager.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MenuApp.Business.Abstracts;
+using MenuApp.Business.Constants;
+using MenuApp.Business.DTOs.Members;
+using MenuApp.Core.Utilities.Results;
+using MenuApp.Core.Utilities.Results.Concrete;
+using MenuApp.DataAccess.Abstracts;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace MenuApp.Business.Concretes
+{
+    public class MemberManager:IMemberService
+    {
+        private readonly IMemberRepository _memberRepository;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public MemberManager(IMemberRepository memberRepository, UserManager<IdentityUser> userManager, IMapper mapper)
+        {
+            _memberRepository = memberRepository;
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        public async Task<IDataResult<MemberDto>> GetByIdentityId(string identityId)
+        {
+            var member = await _memberRepository.GetAsync(x => x.IdentityId == identityId);
+            if (member is null)
+            {
+                return new ErrorDataResult<MemberDto>(Messages.UserNotFound);
+            }
+            return new SuccessDataResult<MemberDto>(_mapper.Map<MemberDto>(member), Messages.ListedSuccess);
+        }
+    }
+}
