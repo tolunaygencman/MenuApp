bffa9366d1f75bdddd616bcd0bcadda44b81b881
Tolunay Gençman <tolunaygencman@hotmail.com>
2023-02-28 16:59:38 +0300

    v.0.1.13 *Added funtcion that creates qr code for display menu

---
+++ MenuApp/MenuApp.MVC/Extensions/QRCodeExtension.cs
@@ -0,0 +1,20 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuApp.MVC.Extensions
+{
+    public class QRCodeExtension
+    {
+        public static byte[] CreatePngQR(string url,int size) 
+        { 
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData pictureData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            PngByteQRCode picture = new PngByteQRCode(pictureData);
+            byte[] bytePicture = picture.GetGraphic(size);
+            return bytePicture;
+        }
+    }
+}
